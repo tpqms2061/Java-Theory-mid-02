207634ec6012bb52225de8c6f66535ac0f11c37e
tpqms2061 <tpqms2061@naver.com>
2025-07-27 23:26:12 +0900

    Linked List toString Overriding

--- src/collection/Linked/Node.java
+++ src/collection/Linked/Node.java
@@ -9,4 +9,29 @@ public class Node {
     public Node(Object item) {
         this.item = item;
     }
+
+//IDE 생성 ,toSTring()
+//   @Override
+//    public String toString() {
+//        return "Node{" +
+//                "item=" + item +
+//                ", next=" + next +
+//                '}';
+//    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Node x =this; // 자기 자신 x01
+        sb.append("[");
+        while (x != null) {
+            sb.append(x.item);
+            if (x.next != null) {
+                sb.append("->");
+            }
+            x = x.next;
+        }
+        sb.append("]");
+        return sb.toString();
+  }
 }
